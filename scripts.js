5aeb3bab1595bbf9b08ec9afb2ba395683a70777
sharjeelyunus <sharjeel924@gmail.com>
2020-08-30 20:05:25 +0500

    📦 NEW: Get Elements

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* Get our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress_filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('player__slider');
+
+/* Build our functions */
+
+/* Hook up event listeners */
